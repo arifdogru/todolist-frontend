758550e3d7b58e2907dbae3600b89453bd9612c2
Arif Dogru <adogru@gt.edu.tr>
2019-10-14 14:45:11 +0300

    d

--- src/apis/dynamicUrl.js
+++ src/apis/dynamicUrl.js
@@ -1,14 +1,15 @@
 let url;
 
-
+/*
 if (process.env.NODE_ENV === "development") {
   console.log(process.env.NODE_ENV);
   url = "http://localhost:22571/v1";
 } else if (process.env.NODE_ENV === "production") {
   console.log(process.env.NODE_ENV);
   url = "https://todolist-backend-arifdogru.herokuapp.com/:31571/v1";
-}
-
+}*/
+console.log(process.env.NODE_ENV);
+url = "https://todolist-backend-arifdogru.herokuapp.com/:31571/v1";
 export default {
   ToDoListServiceUrl: url
 };
